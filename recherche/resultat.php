62961bf206bbd32de2b7b37b3893e69d08569b81
nindayoukonate <emma.konate@gmail.com>
2020-11-30 10:11:21 +0100

    modif formulaire

--- recherche/resultat.php
+++ recherche/resultat.php
@@ -11,8 +11,32 @@
 		echo $_POST[critere4];
 		// TEST DE LA PRESENCE DES VARIABLES RECUPEREES
 		$sql = 'emma'; // requete a faire en onction du choix de l'utilisateur
-		if(isset $_POST[recherche]!='')
-			$sql=
+		if(isset $_POST[recherche]){
+
+			$sql='';
+		}
+		if (!isset($_POST[critere1]) And isset($_POST[critere2]) and !isset($_POST[critere3]) and !isset($_POST[critere4])){
+			$sql='';
+		}
+		if (!isset($_POST[critere1]) And !isset($_POST[critere2]) and isset($_POST[critere3]) and !isset($_POST[critere4])){
+			$sql='';
+		}
+		if (!isset($_POST[critere1]) And !isset($_POST[critere2]) and !isset($_POST[critere3]) and isset($_POST[critere4])){
+			$sql='';
+		}
+		if (isset($_POST[critere1]) And !isset($_POST[critere2]) and !isset($_POST[critere3]) and !isset($_POST[critere4])){
+			$sql='';
+		}
+		if (isset($_POST[critere1]) And !isset($_POST[critere2]) and !isset($_POST[critere3]) and !isset($_POST[critere4])){
+			$sql='';
+		}
+		if (isset($_POST[critere1]) And !isset($_POST[critere2]) and !isset($_POST[critere3]) and !isset($_POST[critere4])){
+			$sql='';
+		}
+		if (isset($_POST[critere1]) And !isset($_POST[critere2]) and !isset($_POST[critere3]) and !isset($_POST[critere4])){
+			$sql='';
+		}
+
 		//connexion a la bdd
 		$bdd= new PDO('mysql:host=localhost;dbname=ProjetA1', 'root', 'root');
 		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
