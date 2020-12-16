249ec167e99171d636f56802ce6e87b322962d7a
nindayoukonate <emma.konate@gmail.com>
2020-12-16 22:35:42 +0100

    Ajout fonctionnalité

--- MENU/connexion.php
+++ MENU/connexion.php
@@ -8,17 +8,17 @@
 			<title>Page de recuperation des données de connexion</title>
 			<?php
 				$sql='';
-
+				$sql2='';
 				$ok=TRUE;
 				if($_POST['mdp']=="") {
 					echo "Le mot de passe n'a pas été saisi"."<br>";
-					echo "<meta http-equiv='refresh'content='3;URL=Menu.html'>";
+					echo "<meta http-equiv='refresh'content='1;URL=Menu.html'>";
 					$ok=FALSE;
 				}
 
 				if($_POST['login']=="") {
 					echo "L'adresse mail n'a pas été saisie"."<br>";
-					echo "<meta http-equiv='refresh'content='3;URL=Menu.php'>";
+					echo "<meta http-equiv='refresh'content='1;URL=Menu.php'>";
 
 					$ok=FALSE;
 				}// GESTION DES ERREURS SUR LE MOT DE PASSE ET L EMAIL
@@ -27,7 +27,7 @@
 					$bdd= new PDO('mysql:host=localhost;dbname=ProjetA1', 'root', 'root');
 
 					$bdd -> setAttribute (PDO :: ATTR_ERRMODE , PDO :: ERRMODE_EXCEPTION );
-					$sql="select email, Mot_De_Passe FROM UTILISATEUR  WHERE Type='admin' AND email LIKE ? AND Mot_De_Passe like?";
+					$sql="select email, Mot_De_Passe, Type FROM UTILISATEUR  WHERE email LIKE ? AND Mot_De_Passe like?";
 					
 					$statement = $bdd -> prepare ( $sql );
 					$statement -> execute (array($_POST['login'],$_POST['mdp']));
@@ -40,22 +40,35 @@
 						echo"<meta http-equiv='refresh'content='3;URL=Menu.php'>";
 					}
 
-
-					else {
+					else{
+					
 					$_SESSION['UTILISATEUR'] = $statement->fetchAll(PDO::FETCH_NUM);
-				
+
+					if($donnees['Type']=='admin' or $donnees['Type']=='gestionnaire'){
 					if (isset($_SESSION['UTILISATEUR'])) {
 
 						$estconnecte = TRUE ;
 						$_SESSION['utilisateurConnecte' ] = $estconnecte;
 						echo ' Vous etes connectés';
-						echo "<meta http-equiv='refresh'content='3;URL=../profiladmin/Profiladmin.php'>";
+						echo "<meta http-equiv='refresh'content='1;URL=../profiladmin/Profiladmin.php'>";//changer la redirection mettre la nouvelle page
 					}
+				}
+
+					elseif($donnees['Type']=='user'){
+					if (isset($_SESSION['UTILISATEUR'])) {
+
+						$estconnecte = TRUE ;
+						$_SESSION['utilisateurConnecte' ] = $estconnecte;
+						echo ' Vous etes connectés';
+						echo "<meta http-equiv='refresh'content='1;URL=../User/User.html'>";//changer la redirection mettre la nouvelle page
+					}
+				}
 					else {
 						$estconnecte =FALSE ;
 						$_SESSION [ 'utilisateurConnecte' ] = $estconnecte ;
-						echo "<meta http-equiv='refresh'content='3;URL=Menu.php'>";
+						echo "<meta http-equiv='refresh'content='1;URL=Menu.php'>";
 						echo 'Vous n etes pas connectes';
+
 					}
 
 				}
