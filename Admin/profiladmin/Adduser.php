4144a1021391514736a332a8c63e0206c0ae8d46
Alex <alexandreliban92@gmail.com>
2020-12-17 22:05:23 +0100

    Add profil admin in Admin

---
+++ Admin/profiladmin/Adduser.php
@@ -0,0 +1,92 @@
+<?php
+// Lancement de la session et récupération des données utilisateur
+
+session_start();/*
+$mail=$_SESSION['mail'];
+$prenom=$_SESSION['prenom'];
+$nom=$_SESSION['nom'];
+$age=$_SESSION['age'];
+$role=$_SESSION['role']; */
+$role = 'administrateur';
+
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=ProjetA1;charset=utf8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+
+// Insertion
+$mailcheck= $bdd->prepare('SELECT * FROM utilisateur WHERE mail= ?');
+$mailcheck->execute(array(htmlspecialchars($_POST['mail'])));
+$result = $mailcheck->fetch();
+$mailcheck = $result[0];
+
+$prenom=htmlspecialchars($_POST['prenom']);
+$nom=htmlspecialchars($_POST['nom']);
+$age=htmlspecialchars($_POST['age']);
+$roole=htmlspecialchars($_POST['role']);
+$mail=htmlspecialchars($_POST['mail']);
+
+if ($role == 'administrateur')
+{
+
+    if (isset($_POST['ajouter']))
+    {
+        if ($mailcheck == 0) {
+            // Ajout de l'utilisateur à l'aide d'une requête préparée
+            $req = $bdd->prepare('INSERT INTO utilisateur (prenom, nom, mdp, age, mail, roole) VALUES(?, ?, ?, ?, ?, ?)');
+            $req->execute(array(htmlspecialchars($_POST['prenom']), htmlspecialchars($_POST['nom']), htmlspecialchars(password_hash($_POST['mdp'], PASSWORD_DEFAULT)), htmlspecialchars($_POST['age']), htmlspecialchars($_POST['mail']), htmlspecialchars($_POST['role'])));
+
+            echo 'L\'utilisateur a bien été ajouté !';
+        } else {
+            echo 'Cette adresse mail est déja utilisé merci d\'utiliser une adresse valide';
+        }
+    }
+    elseif (isset($_POST['modifier']))
+    {
+        if ($mailcheck == 0)
+        {
+            echo 'Vous ne pouvez pas modifier un profil inexistant';
+        }
+        else
+        {
+            $req = $bdd->prepare('UPDATE utilisateur SET prenom = :nvprenom, nom = :nvnom, age = :nvage, roole= :nvroole  WHERE mail=:mail');
+
+            $req->execute(array(
+
+                'nvprenom' => $prenom,
+
+                'nvnom' => $nom,
+
+                'nvage' => $age,
+
+                'nvroole' => $roole,
+
+                'mail' => $mail
+            ));
+
+            echo 'Profil édité avec succès';
+
+        }
+    }
+    elseif (isset($_POST['supprimer']))
+    {
+        if ($mailcheck !== 0) {
+            // Suppression de l'utilisateur à l'aide d'une requête préparée
+            $req = $bdd->prepare('DELETE FROM utilisateur WHERE mail= ?');
+            $req->execute(array(htmlspecialchars($_POST['mail'])));
+            $req->closeCursor();
+            echo 'Utilisateur supprimé !';
+        }
+    else {
+            echo 'Ce profil n\'est pas attribué il ne peut donc pas être supprimée merci d\'entrer une adresse valide';
+        }
+    }
+}
+else{
+    echo 'La modération des profils est réservée aux administrateur';
+}
+
---
+++ Admin/profiladmin/Profiladmin.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" type="text/css" href="profiladmin.css">
+	<title>Profil Admin</title>
+</head>
+
+<body>
+
+
+<div class="content">
+    <header>
+        <div id="box_0">
+            <div class="box_0_element">
+                <ul>
+                    <li>
+                        <a href="admin.html">
+                            <image src= "logo-alex_Plan de travail 1.png" title="Menu"></image>
+                        </a>
+                   </li>
+                </ul>
+                
+            </div>
+            <div class="box_0_element"><a href="../Aide/aide.html" style="color:black" title="Aide">Lombroso </a></div>
+            <div class="box_0_element">
+                <ul>
+                    <li>
+                        <a href="../../profiladmin/Profiladmin.php">
+                            <image src= "user.png" title="Profil"></image>
+                        </a>
+                   </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div class="under_header">
+        
+        <div class="aside_nav">
+            <section>
+                
+				<form action="Adduser.php" method="post">
+                <p><input type="text" name="mail" id="mail" placeholder="mail" /></p>
+				<p><input type="text" name="nom" id="nom" placeholder="NOM" /></p>
+				<p><input type="text" name="prenom" id="prenom" placeholder="Prénom" /> </p>
+				<p><input type="text" name="mdp" id="mdp" placeholder="Mot de passe"/></p>
+                <p><input type="text" name="age" id="age" placeholder="Âge" /></p>
+				<p><input type="text" name="test" id="test" placeholder="Dernier test effectué le :"/></p>
+				<p><input type="text" name="resultat" id="resultat" placeholder="Résultat du dernier test :"/></p>
+				<p><select name="role" id="role" placeholder="Rôle">
+					<option value="admin">Administrateur</option>
+					<option value="testeur">Testeur</option>
+					<option value="teste">Testé</option>
+				</select></p>
+				<p><input type="submit" value="Ajouter ce profil" name="ajouter" id="ajouter" /></p>
+                    <input type="submit" value="Supprimer ce profil" name="supprimer" id="supprimer" />
+                    <input type="submit" value="Enregistrer les modifications" name="modifier" id="modifier" />
+				</form>
+                
+            </section>
+        </div>
+    </div>
+    <footer>
+        <div id="box_1">
+            <div class="box_1_element"><a href="../CGU2/CGU.html" style="color:black" title="CGU">Consulter les CGU</a></div>
+            <div class="box_1_element">
+                <ul>
+                    <li>
+                        <a href="../mail/mail.html">
+                            <image src= "email.png" title="Mail"></image>
+                        </a>
+                   </li>
+                </ul>
+            </div>
+            <div class="box_1_element">
+                <ul>
+                    <li>
+                        <a href="../FAQ/FAQ.html">
+                            <image src= "help.png" title="FAQ"></image>
+                        </a>
+                   </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</div>
+</body>
