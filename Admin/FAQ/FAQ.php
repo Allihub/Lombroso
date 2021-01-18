3a0f49b205a4abf5813c09fc9f0b72f0e84c7d51
Alex <alexandreliban92@gmail.com>
2021-01-18 12:40:15 +0100

    test mail faq
    test

---
+++ Admin/FAQ/FAQ.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="FAQ.css">
+    <title>FAQ</title>
+</head>
+
+<body>
+<!-- <a href="page_8.html" class="change_page"></a> -->
+
+<div class="content">
+    <header>
+        <div id="box_0">
+            <div class="box_0_element">
+                <ul>
+                    <li>
+                        <a href="../Accueil/admin.php">
+                            <image src= "logo-alex_Plan de travail 1.png" title="Menu"></image>
+                        </a>
+                   </li>
+                </ul>
+                
+            </div>
+            <div class="box_0_element"><a href="../Aide/aide.html" style="color:black" title="Aide">Lombroso </a></div>
+            <div class="box_0_element">
+                <ul>
+                    <li>
+                        <a href="../profiladmin/profiladmin.php">
+                            <image src= "user.png" title="Profil"></image>
+                        </a>
+                   </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div class="under_header">
+        
+        <div class="aside_nav">
+            <section>
+                
+                <section class="faq-section">
+                    <input type="checkbox" id="q1">
+                    <label for="q1">Que faire si le test ne se lance pas ou si le test ne fonctionne pas subitement lors d’un test ?</label>
+                    <p>... Dans un premier temps relancez le test ...</p>
+                    <p>... Si le problème persiste vous pouvez contacter un administrateur via l’onglet contact prévu à cet effet ...</p>
+        
+                </section>
+                <section class="faq-section">
+                    <input type="checkbox" id="q2">
+                    <label for="q2">Comment contacter l’administrateur ?</label>
+                    <p>... Via la page de contact ...</p>
+                    <p>... Elle est accessible depuis la page des CGU. ...</p>
+                </section>
+        
+                <section class="faq-section">
+                    <input type="checkbox" id="q3">
+                    <label for="q3">Puis-je faire passer un test à plusieurs utilisateurs en même temps ?</label>
+                    <p>... Oui. ...</p>
+                    <p>... L’application est prévue pour être utilisée par plusieurs utilisateurs sans problème ...</p>
+                </section>
+        
+                <section class="faq-section">
+                    <input type="checkbox" id="q4">
+                    <label for="q4">Le Lombroso est-il adapté à tous les usages ?</label>
+                    <p>... Le Lombroso est un détecteur de mensonge ...</p>
+                    <p>... Le Lombroso est un détecteur de mensonge prévu pour des applications dans le domaine militaire et juridique, ainsi nous vous conseillons de vérifier vos autorisations avant d’utiliser un tel dispositif. ...</p>
+                </section>
+        
+                <section class="faq-section">
+                    <input type="checkbox" id="q5">
+                    <label for="q5">Existe-t-il un support pour la maintenance du Lombroso ?</label>
+                    <p>... Oui. ...</p>
+                    <p>... Effectivement la société Infinite Measures assure toute la maintenance du dispositif.Toutes les informations sont renseignées dans l’onglet contact. ...</p>
+                </section>
+
+                <section class="faq-section">
+                    <input type="checkbox" id="q6">
+                    <label for="q6">Le site permet un accès direct aux statistiques des différents tests ?</label>
+                    <p>... Oui. ...</p>
+                    <p>... Le Lombroso est prévu pour vous afficher tous les résultats ainsi que les statistiques obtenues lors des tests précédents. ...</p>
+                </section>
+
+                <section class="faq-section">
+                    <input type="checkbox" id="q7">
+                    <label for="q7">Le site web prévoit-il une messagerie interne pour le personnel ?</label>
+                    <p>... Non... ...</p>
+                    <p>... Le site web du Lombroso ne prévoit pas encore de messagerie interne ...</p>
+                </section>
+                
+            </section>
+        </div>
+    </div>
+    <footer>
+    <form method="POST" action="Merci.php">
+        <input type ="text" id="poser" placeholder="Poser une question" name="question"> <input type="submit">
+    </form>
+    </footer>
+    <footer>
+        <div id="box_1">
+            <div class="box_1_element"><a href="../CGU2/CGU.html" style="color:black" title="CGU">Consulter les CGU</a></div>
+            <div class="box_1_element">
+                <ul>
+                    <li>
+                        <a href="../mail/mail.html">
+                            <image src= "email.png" title="Mail"></image>
+                        </a>
+                   </li>
+                </ul>
+            </div>
+            <div class="box_1_element">
+                <ul>
+                    <li>
+                        <a href="../FAQ/FAQ.html">
+                            <image src= "help.png" title="FAQ"></image>
+                        </a>
+                   </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</div>
+
+<!-- <a href="page_10.html" class="change_page"></a> -->
+</body>
+</html>
