20d5c5913f608c9bf837d76f83e76f417fc9cda7
Alex <alexandreliban92@gmail.com>
2021-02-01 10:28:32 +0100

    Amélioration aide gros

--- Admin/Accueil/admin.php
+++ Admin/Accueil/admin.php
@@ -27,7 +27,7 @@
 		
         echo "<a href='../../MENU/deconnexion.php' >Se déconnecter</a>"."<br>";
         
-        ?><br><br><a href="../Aide/aide.html" style="color:black" title="Aide">Lombroso </a></div>
+        ?><br><br><a href="../Aide/aide.html" style="color:black;font-size:30px" title="Aide">Aide </a></div>
             <div class="box_0_element">
                 <ul>
                     <li>
